b9cd51fef6dfeb7181a38ae44af3c44f7d157e97
guilhermemonte31 <120525923+guilhermemonte31@users.noreply.github.com>
2022-12-15 14:48:08 -0400

    Add files via upload

---
+++ 20221215_007.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+
+    float tempFaren, tempCelsius;
+
+    printf("informe a temperatura em fahrenheit: ");
+    scanf("%f",&tempFaren);
+
+    tempCelsius = (5*(tempFaren - 32))/9;
+
+    printf("%.1f F sera %.1f C", tempFaren, tempCelsius);
+	return 0;
+
+}
