9604d0783456f4b15414296ed01af07a04e9c100
guilhermemonte31 <120525923+guilhermemonte31@users.noreply.github.com>
2022-12-27 18:25:00 -0300

    Update 20222220005.c

--- 20222220005.c
+++ 20222220005.c
@@ -2,13 +2,36 @@
 #include <stdlib.h>
 int main() {
 
-    int num1, num2;
-	
-	for(num1=1; num1<=9; num1++){
-		for(num2=0; num2<=10; num2++){
-			printf("%d x %d = %d  \n", num1, num2, num1*num2);
+    int num, cont, maior, menor, teste;
+    
+    printf("digite um numero: ");
+    scanf("%d", &teste);
+    
+    maior = teste;
+    menor = teste;
+    if(teste<0){
+		printf("nao e permitido numeros negativos, programa encerrado");
+	}
+	else{
+		for(cont =1; cont<=4; cont++){
+			printf("digite um numero: ");
+	    	scanf("%d", &num);
+	    	if(num<0){
+				printf("nao e permitido numeros negativos, programa encerrado \n");
+				break;
+			}
+	    	else{
+		    	if(num >maior){
+					maior = num;
+				}else{
+					if(num<menor){
+						menor = num;
+					}
+				}
+			}
 		}
-		printf("--------------- \n");
+		
+		printf("maior: %d menor: %d", maior, menor);		
 	}
     
     
