5e1f21ea0184f94946e2fd985f627b99cbda8c0b
guilhermemonte31 <120525923+guilhermemonte31@users.noreply.github.com>
2022-12-27 18:23:35 -0300

    Update 20222220004.c

--- 20222220004.c
+++ 20222220004.c
@@ -6,36 +6,24 @@ int main() {
     
     printf("digite um numero: ");
     scanf("%d", &teste);
-    
     maior = teste;
     menor = teste;
-    if(teste<0){
-		printf("nao e permitido numeros negativos, programa encerrado");
-	}
-	else{
-		for(cont =1; cont<=4; cont++){
-			printf("digite um numero: ");
-	    	scanf("%d", &num);
-	    	if(num<0){
-				printf("nao e permitido numeros negativos, programa encerrado \n");
-				break;
-			}
-	    	else{
-		    	if(num >maior){
-					maior = num;
-				}else{
-					if(num<menor){
-						menor = num;
-					}
-				}
+    
+    for(cont =1; cont<=19; cont++){
+		printf("digite um numero: ");
+    	scanf("%d", &num);
+    	if(num >maior){
+			maior = num;
+		}else{
+			if(num<menor){
+				menor = num;
 			}
 		}
-		
-		printf("maior: %d menor: %d", maior, menor);		
 	}
-    
-    
+	printf("maior: %d menor: %d", maior, menor);
     
 	return 0;
 
 }
+
+
