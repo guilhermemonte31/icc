53e313c1dd5d29477745625e5fb3a136b48e6f68
guilhermemonte31 <120525923+guilhermemonte31@users.noreply.github.com>
2023-02-13 19:19:59 -0400

    Add files via upload

--- 2023011001.c
+++ 2023011001.c
@@ -1,48 +1,29 @@
 #include <stdio.h>
-#define TRUE 1
-#define FALSE 0
+#include <stdlib.h>
 
-int encaixa(int a, int b) {
-  
-  while (b != 0 && a % 10 == b % 10) {
-    a = a/10;
-    b = b/10;
-  }
+int funcao(int n, int d){
+    int cont = 0, resto;
 
-  if (b == 0)
-    return TRUE;
-  else
-    return FALSE;
+    while(n) {
+        resto = n%10;
+        if(resto == d){
+            cont+= 1;
+        }
+        n /= 10;
+    }
 
+
+    return cont;
 }
 
-int main() {
-  int a, b, subseq, maior, menor;
-
-  printf("Digite dois inteiros positivos: ");
-  scanf("%d %d", &a, &b);
-  
-  if (a > b) {
-    maior = a;
-    menor = b;
-  }
-  else {
-    maior = b;
-    menor = a;
-  }    
-
-  subseq = FALSE;
-  while (maior >= menor) {
-    if (encaixa(maior,menor) == TRUE)
-      subseq = TRUE;
-    maior = maior/10;
-  }
-  if (subseq == TRUE)
-    printf("%d e' segmento de %d\n",b,a);
-  else
-    printf("%d nao e' segmento de %d\n",b,a);
-
-  return 0;
+int main()
+{
+    int n, d, repete;
+    printf("digite os valores para n e d: ");
+    scanf("%d %d", &n, &d);
+    repete = funcao(n, d);
+    printf("O numero %d apareceu %d vezes", d, funcao(n, d));
+    return 0;
 }
 
 
