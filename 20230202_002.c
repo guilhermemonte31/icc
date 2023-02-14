735fd1d199311291311e9160ef2b86ff78d792b4
guilhermemonte31 <120525923+guilhermemonte31@users.noreply.github.com>
2023-02-14 12:21:06 -0400

    Update 20230202_002.c

--- 20230202_002.c
+++ 20230202_002.c
@@ -1,72 +1,92 @@
-/*20230202_002 Implementar uma função que receba uma lista de nomes inseridos pelo usuário
- (até 20 nomes com até 120 caracteres). O usuário pode inserir desde zero até 20 nomes.  Criar funções para;
+/*20230202_002 Implementar uma funÃ§Ã£o que receba uma lista de nomes inseridos pelo usuÃ¡rio
+ (atÃ© 20 nomes com atÃ© 120 caracteres). O usuÃ¡rio pode inserir desde zero atÃ© 20 nomes.  Criar funÃ§Ãµes para;
 a) imprimir a lista de nomes
 b) Eliminar um nome da lista.
-c) Imprimir os nomes da lista em ordem alfabética.
-d) Inserir os nomes da lista em outra lista em ordem alfabética.*/
-
+c) Imprimir os nomes da lista em ordem alfabÃ©tica.
+d) Inserir os nomes da lista em outra lista em ordem alfabÃ©tica.*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #define max 120
 
-
-
-void imprime(char lista[][max], int tamanho){
-	int i;;
-	for(i=0; i<tamanho; i++){
-		printf("%s\n", lista[i]);
-	}
-}
-
-void elimina(char lista[][max], int tamanho){
-	int i, pos;
-	
-	printf("Digite a posicao do nome que voce deseja eliminar: ");
-	scanf("%i", &pos);
-	printf("\nNova lista: \n");
-	for(i=0;i<tamanho;i++){
-		if(i!=(pos-1)){
-			printf("%s ", lista[i]);
-		}
-	}
-}
-
-void ordem(char lista[][max], tamanho){
-	int i;
-	char primeiro
-	
-	for(i=0; i<tamanho; i++){
-	}
-}
-
-
-void listarNomes(){
-	char lista[max][max];
-	int j, tamanho;
-	
-	
-	printf("Insira a quantidade de nomes (de 0 a 20): ");
-	scanf("%i", &tamanho);
+void listarNomes(char lista[max][max], int tamanho){
+	int j;
 	
 	for(j=0; j<tamanho; j++){
 		printf("Insira o %d nome: ", j+1);
-		scanf(" %[^\n]s", &lista[j]);	
+		scanf(" %[^\n]s", lista[j]);	
 	}
 	
 	
-	imprime(lista, tamanho);
-	elimina(lista, tamanho);
-	
-	
 }
+
+void imprime(char lista[max][max], int tamanho){
+    int i;
+
+    for(i=0; i<tamanho; i++){
+        printf(" %s\n", lista[i]);
+    }
+}
+
+void elimina(char lista[max][max], int tamanho){
+    int i, j;
+
+    printf("Digite a posicao do numero que voce deseja eliminar: ");
+    scanf("%i", &i);
+
+    i-=1;
+
+    for(j=0; j<tamanho; j++){
+        if(j == i){
+            lista[i][0] = '\0';
+        }
+    }
+    printf("\nNova lista\n");
+    for(j=0; j<tamanho;j++){
+        printf("%s", lista[j]);
+    }
+}
+
+void ordem(char lista[max][max], int tamanho){
+        
+    }
 
 int main(){
 	char a[max][max];
-	int j;
+	int j, tamanho;
+    char escolha;
 	
-	listarNomes();
-		
-}
+	
+	printf("Insira a quantidade de nomes (de 0 a 20): ");
+	scanf("%i", &tamanho);;
 
+
+    printf("Escolha uma opcao: \n");
+    printf("a) imprimir a lista de nomes \nb) Eliminar um nome da lista. \nc) Imprimir os nomes da lista em ordem alfabÃ©tica. \nd) Inserir os nomes da lista em outra lista em ordem alfabÃ©tica.\n e) Encerrar.\n");
+    scanf(" %c", &escolha);
+
+    while(escolha != 'e' || escolha != 'E'){
+        if(escolha == 'a' || escolha == 'A'){
+            imprime(a, tamanho);
+        }else{
+            if(escolha == 'b' || escolha == 'B'){
+                elimina(a, tamanho);
+            }/*else{
+                if(escolha == 'c' || escolha == 'C'){
+                    ordem(a, tamanho);
+                }else{
+                    if(escolha == 'd' || escolha == 'D'){
+                        novaLista(a, tamanho);
+                    }else{
+                        break;
+                    }
+                }
+            */
+        }
+        printf("\nInsira uma nova opcao (para encerrar, digite 'E'): ");
+        scanf(" %c",&escolha);
+
+    }
+
+}
